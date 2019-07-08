09b6d2827d622a63f1033f17addef32c39d0f9bb
madhu16p222 <48043809+madhu16p222@users.noreply.github.com>
2019-07-08 16:20:46 +0530

    Update n.py

--- n.py
+++ n.py
@@ -1,1 +1,28 @@
+character = 256
+def max(str, n):
+    count = [0] * character
+    for i in range(n):
+        count[ord(str[i])] += 1
+    max_distinct = 0
+    for i in range(character):
+        if (count[i] != 0):
+            max_distinct += 1	
+    return max_distinct 
+
+def Substr(str):
+    n = len(str)	
+    max_distinct = max(str, n) 
+    num = n	 
+    for i in range(n):
+        for j in range(n):
+            subs = str[i:j]
+            su_l = len(subs) 
+            su_char = max(subs, su_l)
+            if (su_l < num and max_distinct == su_char):
+                num = su_l
+    return num
+
+string=input()
+result=Substr(string)
+print(result)
 
